5f31b1d136023ea6c8c35f2c10192c713989bf7d
Joe Burke <joe@clinkle.com>
2013-09-07 16:00:20 -0700

    first commit to develop, new project

---
+++ ChevroletPassProject/ChevroletPass/src/main/java/com/chevroletpass/DashActivity.java
@@ -0,0 +1,23 @@
+package com.chevroletpass;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dash_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dash, menu);
+        return true;
+    }
+    
+}
